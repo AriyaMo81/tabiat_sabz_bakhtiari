e659d5a7ab266715b455ebe883198bb7ab146746
ary.mo <mokhtarpourariya95@gmail.com>
2026-09-03 14:40:38 +0330

    add about section

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home.index');
-});
+})->name('home.index');
 
+
+Route::get('about-us', function () {
+    return view('about_us');
+})->name('about.index');
