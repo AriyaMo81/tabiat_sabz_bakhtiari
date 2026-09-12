8327948af96040598634815bad721d33e2ad66ca
ary.mo <mokhtarpourariya95@gmail.com>
2026-09-12 22:57:33 +0330

    add menu section

--- laravel-admin-panel/app/Models/Product.php
+++ laravel-admin-panel/app/Models/Product.php
@@ -5,19 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Category;
+use App\Models\ProductImage;
 
 class Product extends Model
 {
     use HasFactory, SoftDeletes;
-
     protected $table = 'products';
     protected $guarded = [];
-
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class)->withTrashed();
     }
-
     public function images()
     {
         return $this->hasMany(ProductImage::class);
